399efdafe1cd31daccec0ab1e2b6be8c620cb20e
neihtfool <q.thien.nguyen@outlook.de>
2019-11-14 02:11:06 +0100

    implement d3

--- main.js
+++ main.js
@@ -1,32 +1,68 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-ctx.stroke()
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+// Parse the date / time
+var	parseDate = d3.time.format("%Y-%m").parse;
 
-function Rectangle(x, y, velX, velY, color, size) {
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-}
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
 
-Rectangle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = "cornflowerBlue";
-    ctx.rect(this.x, this.y, 25, 10);
-    ctx.fill()
-}
+var y = d3.scale.linear().range([height, 0]);
 
-Rectangle.prototype.update = function() {
-    this.y += this.velY
-}
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.time.format("%Y-%m"));
 
-function loop() {
-    testRect.draw()
-    testRect.update()
-    requestAnimationFrame(loop)
-}
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("bar-data.csv", function(error, data) {
+
+    data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d.value = +d.value;
+    });
+
+    x.domain(data.map(function(d) { return d.date; }));
+    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", "-.55em")
+        .attr("transform", "rotate(-90)" );
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Value ($)");
+
+    svg.selectAll("bar")
+        .data(data)
+        .enter().append("rect")
+        .style("fill", "steelblue")
+        .attr("x", function(d) { return x(d.date); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.value); })
+        .attr("height", function(d) { return height - y(d.value); });
+
+});
