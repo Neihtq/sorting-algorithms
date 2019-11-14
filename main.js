398171a0683283015b8c987c3edc7fadb5ead6aa
neihtfool <q.thien.nguyen@outlook.de>
2019-11-14 11:26:28 +0100

    implement sorting animation

--- main.js
+++ main.js
@@ -1,68 +1,126 @@
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+var myData = [800, 100, 200, 320, 50, 500, 300, 115];
 
-// Parse the date / time
-var	parseDate = d3.time.format("%Y-%m").parse;
+var margin = {
+    top: 30,
+    right: 30,
+    bottom: 40,
+    left: 50
+};
+var tmp = myData.slice();
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+tmp.sort(function (a,b) {
+    return a - b;
+});
 
-var y = d3.scale.linear().range([height, 0]);
+var colorPalette = {};
+for (i = 0; i < tmp.length; i++) {
+    colorPalette[tmp[i]] = i
+}
+
+var height = 1080 - margin.top - margin.bottom;
+var width = 1920 - margin.right - margin.left;
+var barWidth = 35;
+var barOffset = 5;
+var animeDuration = 700;
+var animateDelay = 30;
+
+var yScale = d3.scale.linear()
+    .domain([0, d3.max(myData)])
+    .range([0, height]);
+
+var xScale = d3.scale.ordinal()
+    .domain(myData)
+    .rangeRoundBands(myData, .1, 1);
+
+var colors = d3.scale.linear()
+    .domain([0, myData.length])
+    .range(['#90ee90', '#30c230']);
+
+var data = [800, 100, 200, 320, 50, 500, 300, 115];
+
+var margin = {top: 50, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatPercent = d3.format("d");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1, 1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%Y-%m"));
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+    .orient("bottom");
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("bar-data.csv", function(error, data) {
+x.domain(data);
+y.domain([0, d3.max(data)]);
 
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.value = +d.value;
-    });
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-    x.domain(data.map(function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.value; })]);
+svg.append("g")
+    .attr("class", "y axis")
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
 
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+svg.append("text")
+    .attr("x", (width / 2))
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px");
+
+svg.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .style('fill', function (d, i) {
+        return colors(colorPalette[d])
+    })
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d); })
+    .attr("width", x.rangeBand())
+    .attr("y", function(d) { return y(d); })
+    .attr("height", function(d) { return height - y(d); })
+
+d3.select("input").on("change", change);
+
+var sortTimeout = setTimeout(function() {
+    d3.select("input").property("checked", true).each(change);
+}, 2000);
+
+function change() {
+    clearTimeout(sortTimeout);
+
+    // Copy-on-write since tweens are evaluated after a delay.
+    var x0 = x.domain(data.sort(this.checked
+        ? function(a, b) { return b - a; }
+        : function(a, b) { return d3.ascending(a, b); })
+        .slice());
+
+    svg.selectAll(".bar")
+        .sort(function(a, b) { return x0(a) - x0(b); });
+
+    var transition = svg.transition().duration(500),
+        delay = function(d, i) { return i * 25; };
+
+    transition.selectAll(".bar")
+        .delay(delay)
+        .attr("x", function(d) { return x0(d); });
+
+    transition.select(".x.axis")
         .call(xAxis)
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", "-.55em")
-        .attr("transform", "rotate(-90)" );
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Value ($)");
-
-    svg.selectAll("bar")
-        .data(data)
-        .enter().append("rect")
-        .style("fill", "steelblue")
-        .attr("x", function(d) { return x(d.date); })
-        .attr("width", x.rangeBand())
-        .attr("y", function(d) { return y(d.value); })
-        .attr("height", function(d) { return height - y(d.value); });
-
-});
+        .selectAll("g")
+        .delay(delay);
+}
